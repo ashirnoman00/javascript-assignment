fcc53c86667fbe0a38071c706fb0b78d1c23d533
muhammad ashir noman <141489855+ashirnoman00@users.noreply.github.com>
2023-09-01 15:33:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+                 //chapter#1//
+/*answer 1
+//alert("hello world")
+
+*answer 2
+//alert('Error!please enter a valid password.')
+
+//*answer 3
+
+//alert('Welcome to JS land...\nHappy coding!')
+
+//*answer 4
+
+//alert('Welcome to JS Land..')
+//alert('Happy coding!\n▢prevent this page from creating additional dialog')
+
+//*answer 5
+
+//alert("Hello...I can run JS through my web brower's console")
+
+   //-----chapter 2-------//
+
+   //*answer 1 and 2 
+   //var name='ashir'
+ //var name2='noman'
+//console.log( name + name2)
+
+ //*answer 3 
+// alert('hello world')
+
+ //*answer 4
+ //alert('m.ashir noman')
+ //alert('age:20 years old')
+ //alert('certified mobile application development')
+
+// *answer 5
+// alert('pizza \npizz \npiz \npi \np')
+    
+// * answer 6
+// var email='abc@gmail.com'
+// alert('my email address is '+email)
+
+// * answer7
+// var book='I am trying To Learn From Smarter Way To Learn JS'
+// alert(book)
+
+// * answer 8  
+// document.writeln('my name is ashir i am a web developer.')
+
+//*answer 9
+
+ //var show_string ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+ //alert(show_string)
+
+
+
